458e05009f4943571b778025178d207879f8c0f7
SufianYM <yousfisoufiane8@gmail.com>
2025-08-20 00:15:19 +0200

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn'
+    },
+  },
+);
